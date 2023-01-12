4713d200a04f50b6e51a2d14c3be74b4db832bf0
Rose van der Giessen <rosevandergiessen@proton.me>
2023-01-12 19:18:42 +0100

    add unit testing to flight app

--- flights/models.py
+++ flights/models.py
@@ -16,6 +16,9 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration > 0
+
 # blank=True gives the possibility for a passenger to have no flights
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
